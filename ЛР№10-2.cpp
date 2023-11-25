2ec93ef07357f8143a1c30c85698102c1f133b4b
Endray93 <150658708+Endray93@users.noreply.github.com>
2023-11-26 00:10:23 +0300

    Add files via upload

---
+++ ЛР№10-2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    double PriceOneEdition; 
+    int NumberPublicationsMonth; 
+    cout << "Введіть вартість одного номера: ";
+    cin >> PriceOneEdition;
+    cout << "Введіть кількість видань за місяць: ";
+    cin >> NumberPublicationsMonth;
+    double yearlyCost = PriceOneEdition * NumberPublicationsMonth * 12;
+    cout << "Вартість річної підписки: " << yearlyCost << endl;
+    return 0;
+}
+
+
+//інший вид запису
